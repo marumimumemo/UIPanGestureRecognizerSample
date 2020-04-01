59c24230774bc51f716b20104ac9ad11e520d6fd
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-04-01 10:16:37 +0900

    UIPanGestureRecognizerSample

--- UIPanGestureRecognizerSample/ViewController.swift
+++ UIPanGestureRecognizerSample/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func PanLabel(_ sender: UIPanGestureRecognizer) {
+        //移動量を取得する。
+        let move:CGPoint = sender.translation(in: view)
+        //ドラッグした部品の座標に移動量を加算する。
+        sender.view!.center.x += move.x
+        sender.view!.center.y += move.y
+        //ラベルに現在座標を表示する。
+        //testLabel.text = "\(sender.view!.frame.origin.x), \(sender.view!.frame.origin.y)"
+        
+        //移動量を0にする。
+        sender.setTranslation(CGPoint.zero, in: view)
+    }
+    
 }
 
